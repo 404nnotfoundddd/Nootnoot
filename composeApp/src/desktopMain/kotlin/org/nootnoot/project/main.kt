d16453613406820ff1c21ef35bf6eb5e6ceb000a
404 <sametalpdeger9@gmail.com>
2025-03-16 18:41:17 +0300

    Design project folder structre

--- composeApp/src/desktopMain/kotlin/org/nootnoot/project/main.kt
+++ composeApp/src/desktopMain/kotlin/org/nootnoot/project/main.kt
@@ -2,6 +2,7 @@ package org.nootnoot.project
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.nootnoot.project.app.App
 
 fun main() = application {
     Window(
